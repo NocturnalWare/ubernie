5d2000d51adc2c996b23af3bbedf80a0837da047
Jota Vares <joe@eternallynocturnal.com.com>
2016-01-22 20:21:35 -0500

    Master commit

---
+++ app/Http/routes.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', ['as' => 'home', 'uses' => function () {
+    return view('welcome');
+}]);
+
+Route::get('/home', function () {
+    return view('welcome');
+});
+
+Route::get('auth/login', array('as' => 'auth.login', 'uses' => 'Auth\AuthController@getLogin'));
+Route::post('auth/login', array('as' => 'auth.login', 'uses' => 'Auth\AuthController@postLogin'));
+
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+Route::group(['middleware' => 'auth'], function () {
+
+
+	});
